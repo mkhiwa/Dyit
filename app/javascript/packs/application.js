8296628b612df6d5d6c2460a0258cb88f5e42c0e
Ricardo <r21mordaunt@gmail.com>
2021-03-12 00:41:02 +0200

    Third commit bootstrap stimulus

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,17 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "bootstrap";
+import "@fortawesome/fontawesome-free/css/all.css"
+import "../stylesheets/application";
+import "controllers"
+
+
+
+$(document).on('ready turbolinks:load', () => {
+    $('[data-toggle="tooltip"]').tooltip();
+    $('[data-toggle="popover"]').popover();
+});
 
 Rails.start()
 Turbolinks.start()
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,6 @@ Rails.application.routes.draw do
     resources :tasks
   end
   devise_for :users
-	root to: "home#index"
+	root to: "projects#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
